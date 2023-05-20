d685dc4ebe4e8bd8a2cffb6c6e678d2038f29cbe
pyer96 <pierluigi.manfrini@gmail.com>
2023-05-20 19:27:47 +0200

    First dsp4sdr word

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <vector>
-
 
 int main(int argc, char** argv){
-
-    std::vector<int> vec_int = {1,2,3};
-    std::cout<< "Hi!" << std::endl; 
+    std::cout<< "Hi dsp4sdr!" << std::endl; 
     return 0;
 }
