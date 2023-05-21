1eb27d9529dcf00c03784381d90edcf27caa85a1
pyer96 <pierluigi.manfrini@gmail.com>
2023-05-21 03:45:14 +0200

    few updates on the HackRf handler class

--- libs/inc/SDR/HackRF.h
+++ libs/inc/SDR/HackRF.h
@@ -1,10 +1,16 @@
+ #include <libhackrf/hackrf.h>
 namespace sdr{
 /**
  * @class HackRF
  * @brief This class represents the APIs for using the HackRF One software defined radio
 */
 class HackRF {
+    public:
+    HackRF();
+    ~HackRF(){_exit_libhackrf();}
     private: 
+    hackrf_device* _device_ptr;
+
     /**
      * @fn _initialize_libhackrf() 
      * @brief This function should be called befory any other libhackrf function,
@@ -19,12 +25,16 @@ class HackRF {
      * @return void
     */
     void _exit_libhackrf(); 
+/**
+ * @fn _handle_device_list
+ * @brief This function checks how many hackrf devices are present in the system.
+ *          If there is more than one device the user is prompted to choose which one
+ *          to interface to. The private member <_device_ptr> is set
+ * @return void
+*/
+void _handle_device_list();
 
 
-
-    public:
-    HackRF();
-    ~HackRF(){_exit_libhackrf();}
 };
 }
 
--- libs/src/SDR/HackRF.cpp
+++ libs/src/SDR/HackRF.cpp
@@ -1,27 +1,51 @@
 #include "../../inc/SDR/HackRF.h"
-#include <libhackrf/hackrf.h>
+#include "../../../utils/useful_code/colors.h"
 #include <iostream>
 namespace sdr{
 
 HackRF::HackRF(){
 /*initialize libhackrf*/
 _initialize_libhackrf(); 
+
+/*detech hackrf devices*/
+_handle_device_list();
 }
 
+
+/*#######################
+    PRIVATE FUNCTIONS
+########################*/ 
 void HackRF::_initialize_libhackrf(){
     if(hackrf_init()==HACKRF_SUCCESS){
-        dprintf(2, "hackrf library initialized...\n");
+        dprintf(2, green ">" reset "\thackrf library initialized...\n");
     }else{
-        dprintf(2,"error while initializing hackrf library...\n");
+        dprintf(2,red ">" reset "\terror while initializing hackrf library...\n");
     };
 }
-
 void HackRF::_exit_libhackrf(){
     if(hackrf_exit()==HACKRF_SUCCESS){
-        dprintf(2, "libhackrf: libhackrf correctly closed...\n");
+        dprintf(2, green ">" reset"\tlibhackrf: libhackrf correctly closed...\n");
     }else{
-        dprintf(2,"libhackrf: not all devices correctly closed...\n");
+        dprintf(2,red ">" reset"\tlibhackrf: not all devices correctly closed...\n");
     };
 }
+void HackRF::_handle_device_list(){
+    hackrf_device_list_t* dev_list = hackrf_device_list();
+    if(dev_list->devicecount>1){
+        printf(green">" reset"\tSDR::HackRF # %d hackrfs detected, which one you want to open?\n", dev_list->usb_devicecount);
+        for(int i = 0; i< dev_list->devicecount; i++){
+            printf(green ">" reset"\t%d) SN: %s\n", i, dev_list->serial_numbers[i]);
+        }
+        int selection; 
+        scanf("%d", &selection);
+        if(selection>=0 && selection < dev_list->devicecount){
+            printf(green">" reset"SDR::HackRF # %s has been selected!\n", dev_list->serial_numbers[selection]);
 
+        }
+        
+    }else{
+        printf(green ">" reset"\tSDR::HackRF # 1 hackrf detected, opening it...\n"); 
+    }
+
+}
 }
